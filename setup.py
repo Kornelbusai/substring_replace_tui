b5c4fd59531ea67ae1c0fdd78bb043e5eef43783
Kornel Busai <kornel.busai@gmail.com>
2024-11-19 22:45:21 +0900

    Update setup.py to comply with better documentation practices. Removed the accidentally left in 'OSI Approved' segment within setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="substring_replacer",  # Name of your package
-    version="1.0.0",
+    ### Metadata
+    name="substring_replacer",  # Name of package
+    version="1.0.0",  # Version of package
     packages=find_packages(),
     py_modules=["substring_replace_script"],  # Python files included
-    install_requires=['argparse', 'setuptools'],  # Dependencies (if any)
+    install_requires=['argparse', 'setuptools'],  # Dependencies 
     entry_points={
         'console_scripts': [
             'substring-replace=substring_replace_script:main',
         ],
     },
-    author="Busai Kornel",
+    author="Busai Kornél",
     author_email="kornel.busai@gmail.com",
     description="Script that removes substring from all file names within a directory.",
     url="https://github.com/Kornelbusai/substring_replace_tui",  # GitHub repository URL
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    license_files = 'MIT'
 )
